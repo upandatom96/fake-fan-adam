43ec1c96d1224ac5688305f302a01bec20675330
Adam Callanan <a.callanan@sbcglobal.net>
2020-04-15 23:41:49 -0500

    main comps working

--- src/app/components/bookmark-details/bookmark-details.component.ts
+++ src/app/components/bookmark-details/bookmark-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Bookmark } from 'src/app/models/Bookmark.model';
+import { BooleanHelper } from 'src/app/utilities/boolean.util';
+import { BookmarkService } from 'src/app/services/bookmark.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-bookmark-details',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookmark-details.component.scss']
 })
 export class BookmarkDetailsComponent implements OnInit {
+  public bookmark: Bookmark = null;
 
-  constructor() { }
+  public get ready(): boolean {
+    return BooleanHelper.notNull(this.bookmark);
+  }
 
-  ngOnInit() {
+  constructor(
+    private bookmarkService: BookmarkService,
+    private route: ActivatedRoute,
+  ) { }
+
+  public ngOnInit() {
+    this.loadApp();
+  }
+
+  private loadApp(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.bookmarkService.getSingle(id)
+      .subscribe((res) => this.bookmark = res,
+        (error) => {
+          console.log("get bookmark failed");
+        });
   }
 
 }
