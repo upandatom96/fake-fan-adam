552c24d34310a2adb8652cad37fbc75764ef7c85
Adam Callanan <a.callanan@sbcglobal.net>
2021-03-14 21:46:33 -0500

    muppet display

--- src/app/components/muppet-display/muppet-display.component.ts
+++ src/app/components/muppet-display/muppet-display.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {MuppetService} from "../../services/muppet.service";
+import {NavHelperService} from "../../services/nav-helper.service";
+import {Muppet} from "../../models/Muppet.model";
 
 @Component({
   selector: 'app-muppet-display',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./muppet-display.component.scss']
 })
 export class MuppetDisplayComponent implements OnInit {
+  public muppet: Muppet = null;
 
-  constructor() { }
+  public get ready(): boolean {
+    return this.muppet !== null;
+  }
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private muppetService: MuppetService,
+    private navHelper: NavHelperService,
+  ) {
+  }
+
+  public ngOnInit() {
+    this.load();
+  }
+
+  public requestDelete(): void {
+    const confirmDelete = confirm(`Are you sure you want to delete ${this.muppet.name}?`);
+    if (confirmDelete) {
+      this.delete();
+    }
+  }
+
+  public edit() {
+    this.navHelper.goToMuppetEdit(this.muppet._id);
+  }
+
+  private delete() {
+    let response;
+    this.muppetService.delete(this.muppet._id)
+      .subscribe((res) => response = res,
+        () => {
+        },
+        () => {
+          this.navHelper.goToMuppets();
+        });
+  }
+
+  private load() {
+    const muppetId = this.route.snapshot.paramMap.get("id");
+    this.muppetService.getSingle(muppetId)
+      .subscribe((res) => this.muppet = res,
+        (error) => {
+          console.log("get single failed");
+        });
   }
 
 }
