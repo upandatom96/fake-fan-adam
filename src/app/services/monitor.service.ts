b32b3621ebc9f09552ac87c48ac8258048856a4e
Adam Callanan <a.callanan@sbcglobal.net>
2022-07-17 15:35:21 -0500

    add summary

--- src/app/services/monitor.service.ts
+++ src/app/services/monitor.service.ts
@@ -28,6 +28,15 @@ export class MonitorService {
     return this.http.get(url, CookieHelper.authHeaders) as Observable<Case[]>;
   }
 
+  public getCaseSummaries(): Observable<string[]> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller: "case",
+      collection: "summary"
+    });
+    return this.http.get(url, CookieHelper.authHeaders) as Observable<string[]>;
+  }
+
   public getClosedCases(): Observable<Case[]> {
     const url = RestUrlBuilder.buildRestUrl({
       service: ServiceUrl.BasicExpress,
